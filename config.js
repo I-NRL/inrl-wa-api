3099ca41568ea2a95003fc30d6fb95f42be55fd7
I-NRL <fasweehmon2@gmail.com>
2024-10-18 20:10:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'inrl~1655fDtIXb19c3454c1f4e5281c97dd7dde9::16', //your ssid to run bot
     PORT: process.env.PORT || 3000,
     BASE_URL : "https://thoughtful-alvera-igpvdr-2dc99e5b.koyeb.app/",
-    PROVIDER_URL: "http://localhost:5000",
+    PROVIDER_URL: "https://indirect-rosamund-inrl-bot-3c87e07a.koyeb.app",
     AUTH: {
         token: "K2E0s2Actc8oDc9wDaRpEW9yuW8oOqRpOqH3O1SEO19wrdkOu1EBseQQIUSJuYAJsn=",
     },
